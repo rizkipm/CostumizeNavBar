f1ec61395ee87e99c33e939f6e8a185468b81b3a
Rizki Syaputra <programmermuslim@gmail.com>
2017-10-10 11:17:16 +0700

    commit new xcdde project

---
+++ Costumize Nav Bar/Costumize Nav Bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Costumize Nav Bar
+//
+//  Created by Rizki Syaputra on 10/10/17.
+//  Copyright © 2017 Rizki Syaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
